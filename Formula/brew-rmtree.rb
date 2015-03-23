172a87bf1a55111fbe70b55e071203d4804df614
Josh Hagins <hagins.josh@gmail.com>
2015-03-23 17:24:02 -0400

    Use double-quotes everywhere

--- Formula/brew-rmtree.rb
+++ Formula/brew-rmtree.rb
@@ -1,14 +1,14 @@
-require 'formula'
+require "formula"
 
 class BrewRmtree < Formula
-  homepage 'https://github.com/beeftornado/homebrew-rmtree'
-  url 'git://github.com/beeftornado/homebrew-rmtree.git'
-  version '1.1'
+  homepage "https://github.com/beeftornado/homebrew-rmtree"
+  url "https://github.com/beeftornado/homebrew-rmtree.git"
+  version "1.1"
 
-  skip_clean 'bin'
+  skip_clean "bin"
 
   def install
-    bin.install 'brew-rmtree.rb'
-    (bin + 'brew-rmtree.rb').chmod 0755
+    bin.install "brew-rmtree.rb"
+    (bin + "brew-rmtree.rb").chmod 0755
   end
 end
